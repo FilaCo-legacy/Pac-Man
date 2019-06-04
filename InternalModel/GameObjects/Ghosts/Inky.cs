55cb8d4348e7893be9f128e8e3e11359cdd47d57
FilaCo <FilaCo159@yandex.ru>
2019-06-04 20:19:52 +0500

    Implemented ghosts

--- InternalModel/GameObjects/Ghosts/Inky.cs
+++ InternalModel/GameObjects/Ghosts/Inky.cs
@@ -2,22 +2,23 @@ using GameServer.GameMap;
 
 namespace GameServer.GameObjects.Ghosts
 {
-    public class Inky: Ghost
+    public class Inky : Ghost
     {
         private static readonly Inky Instance = new Inky();
 
         public static Inky GetInstance => Instance;
-        
+
         public override MapPoint TargetPointScatter => new MapPoint(35, 27);
 
         public override MapPoint TargetPointChase
         {
             get
             {
-                var pacManPos = PacMan.GetInstance.Position;
                 var blinkyPos = Blinky.GetInstance.Position;
+                var centerSegment = PacMan.GetInstance.Position;
 
-                var centerSegment = pacManPos[PacMan.GetInstance.Direction][PacMan.GetInstance.Direction];
+                for (var i = 0; i < 2; ++i)
+                    centerSegment = centerSegment[PacMan.GetInstance.Direction];
 
                 return centerSegment + (centerSegment - blinkyPos);
             }
@@ -25,6 +26,7 @@ namespace GameServer.GameObjects.Ghosts
 
         private Inky()
         {
-            
+
         }
+    }
 }
--- InternalModel/GameObjects/Ghosts/Pinky.cs
+++ InternalModel/GameObjects/Ghosts/Pinky.cs
@@ -1,7 +1,32 @@
+using System;
+using GameServer.GameMap;
+
 namespace GameServer.GameObjects.Ghosts
 {
-    public class Pinky
+    public class Pinky : Ghost
     {
-        
+        private static readonly Pinky Instance = new Pinky();
+
+        public static Pinky GetInstance => Instance;
+
+        public override MapPoint TargetPointScatter => new MapPoint(0, 2);
+
+        public override MapPoint TargetPointChase
+        {
+            get
+            {
+                var targetPos = PacMan.GetInstance.Position;
+
+                for (var i = 0; i < 4; ++i)
+                    targetPos = targetPos[PacMan.GetInstance.Direction];
+
+                return targetPos;
+            }
+        }
+
+        private Pinky()
+        {
+
+        }
     }
 }
