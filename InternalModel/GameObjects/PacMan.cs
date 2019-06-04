30354f82862e09e9e954d54e673c33b8ceaa837b
FilaCo <FilaCo159@yandex.ru>
2019-06-04 23:19:25 +0500

    Implemeted pacman's direction changing

--- InternalModel/GameObjects/PacMan.cs
+++ InternalModel/GameObjects/PacMan.cs
@@ -7,18 +7,34 @@ namespace GameServer.GameObjects
     {
         private static readonly PacMan Instance = new PacMan();
 
+        private MoveDirection _direction;
+
+        private MoveDirection _requestDirection;
+        
         public static PacMan GetInstance => Instance;
         
-        protected override int Ticks => 30;
-        
-        protected override bool CanMove(MapPoint targetPoint)
+        protected override int MillisecondsSkip => 150;
+
+        public override MoveDirection Direction
         {
-            return base.CanMove(targetPoint) && Map.GetInstance[targetPoint] != MapObjCode.Door;
+            get => CanMove(Position[_requestDirection]) ? _direction = _requestDirection : _direction;
+            set
+            {
+                if (CanMove(Position[value]))
+                    _direction = value;
+
+                _requestDirection = value;
+            }
         }
 
         private PacMan()
         {
             Position = Map.GetInstance.StartPacManLocation;
+        }
+        
+        protected override bool CanMove(MapPoint targetPoint)
+        {
+            return base.CanMove(targetPoint) && Map.GetInstance[targetPoint] != MapObjCode.Door;
         }
     }
 }
